eb3eeb2916ba86f4c51dd958aa52488606adc86e
ObjectIsAdvantag <steve.sfartz@gmail.com>
2017-11-07 21:16:44 +0100

    fix for quiz skill

--- skills/quiz.js
+++ skills/quiz.js
@@ -4,7 +4,7 @@
 //
 module.exports = function (controller) {
 
-    controller.hears([/^color$/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^quiz$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
 
